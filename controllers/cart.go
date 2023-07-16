641a3d9d641f6d0b7983e7015697a8f9516cb974
ChiHaoLu <b08303113@ntu.edu.tw>
2023-07-16 20:45:36 +0800

    sec

---
+++ controllers/cart.go
@@ -0,0 +1,26 @@
+package controllers
+
+import(
+
+)
+
+func AddToCart() gin.Handler {
+
+
+}
+
+func RemoveItem() gin.HandlerFunc {
+
+}
+
+func GetImemFromCart() gin.HandlerFunc {
+
+}
+
+func BuyFromCart() gin.HandlerFunc {
+
+}
+
+func InstantBuy() gin.HandlerFunc {
+
+}
---
+++ controllers/controllers.go
@@ -0,0 +1,33 @@
+package controllers
+
+import(
+
+)
+
+func HashPassword(password) string{
+
+} 
+
+func VerifyPassword(userPassword string, givenPassword string) (bool, string){
+
+}
+
+func Signup( ) gin.HandlerFunc{
+
+}
+
+func Login( ) gin.HandlerFunc{
+
+}
+
+func ProductViewerAdmin( ) gin.HandlerFunc{
+
+}
+
+func searchProduct() gin.HandlerFunc {
+
+}
+
+func searchProductByQuery() gin.HandlerFunc {
+
+}
